3025a7575f84e7609392cf9a6b816a062a212972
alanah2004 <a.hartley7738@student.leedsbeckett.ac.uk>
2023-10-27 14:28:52 +0100

    changing celsius to fahrenheit

--- week 1 portfolio.py
+++ week 1 portfolio.py
@@ -1,2 +1,8 @@
-name = input("what is your name?")
-print("Hello", name)
+if __name__ == '__main__':
+
+
+    temperature_in_celsius = float(input("temperature to change"))
+
+    temperature_in_Fahrenheit = (temperature_in_celsius * 1.8) + 32
+
+    print(str(temperature_in_celsius) + " Degrees in celsius is equal to " + str(temperature_in_Fahrenheit) + " degree fahrenheit")
